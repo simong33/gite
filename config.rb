d392c47c95232bc34af67260ec6034a127fa5f17
Simon Guillorit <simon.guillorit@gmail.com>
2017-10-15 20:17:19 +0200

    front

--- config.rb
+++ config.rb
@@ -13,6 +13,7 @@ configure :build do
   activate :minify_javascript
   activate :asset_hash
   activate :relative_assets
+  activate :directory_indexes
   set :relative_links, true
 end
 
---
+++ source/javascripts/home.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+    $('.home-content h1').fadeIn(2000).removeClass('hidden');
+});
