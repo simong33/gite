da0c740a4eac740330b07ff88fbb0a1d21ca4611
Simon Guillorit <simon.guillorit@gmail.com>
2017-10-18 14:02:31 +0200

    rooms + footer

--- source/javascripts/home.js
+++ source/javascripts/home.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
     $('.home-content h1').fadeIn(2000).removeClass('hidden');
+    // Activate Carousel
+    $("#myCarousel").carousel();
+
+    // Enable Carousel Indicators
+    $(".item").click(function(){
+        $("#myCarousel").carousel(1);
+    });
+
+    // Enable Carousel Controls
+    $(".left").click(function(){
+        $("#myCarousel").carousel("prev");
+    });
 });
